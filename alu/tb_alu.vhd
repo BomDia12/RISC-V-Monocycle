c8bd7631bb14eaed74d6372d57091014aacdfb9f
Arthur Mota Furtado <arthur.mota@syngenta.com>
2023-11-25 12:07:56 -0300

    tested bitwise operations

--- alu/tb_alu.vhd
+++ alu/tb_alu.vhd
@@ -56,6 +56,99 @@ architecture test of tb_alu is
     constant sub_r3   : std_logic_vector (31 downto 0) := x"0000" & x"00f5";
     constant sub_z3   : std_logic                      := '0';
 
+    -- And test values
+    constant and_a1   : std_logic_vector (31 downto 0) := x"abcd" & x"ef01";
+    constant and_b1   : std_logic_vector (31 downto 0) := x"0000" & x"ffff";
+    constant and_r1   : std_logic_vector (31 downto 0) := x"0000" & x"ef01";
+    constant and_z1   : std_logic                      := '0';
+    constant and_a2   : std_logic_vector (31 downto 0) := x"1234" & x"5678";
+    constant and_b2   : std_logic_vector (31 downto 0) := x"0000" & x"0000";
+    constant and_r2   : std_logic_vector (31 downto 0) := x"0000" & x"0000";
+    constant and_z2   : std_logic                      := '1';
+    constant and_a3   : std_logic_vector (31 downto 0) := x"1234" & x"5678";
+    constant and_b3   : std_logic_vector (31 downto 0) := x"ffff" & x"ffff";
+    constant and_r3   : std_logic_vector (31 downto 0) := x"1234" & x"5678";
+    constant and_z3   : std_logic                      := '0';
+
+    -- Or test values
+    constant or_a1   : std_logic_vector (31 downto 0) := x"abcd" & x"ef01";
+    constant or_b1   : std_logic_vector (31 downto 0) := x"0000" & x"ffff";
+    constant or_r1   : std_logic_vector (31 downto 0) := x"abcd" & x"ffff";
+    constant or_z1   : std_logic                      := '0';
+    constant or_a2   : std_logic_vector (31 downto 0) := x"0000" & x"0000";
+    constant or_b2   : std_logic_vector (31 downto 0) := x"0000" & x"0000";
+    constant or_r2   : std_logic_vector (31 downto 0) := x"0000" & x"0000";
+    constant or_z2   : std_logic                      := '1';
+    constant or_a3   : std_logic_vector (31 downto 0) := x"1234" & x"5678";
+    constant or_b3   : std_logic_vector (31 downto 0) := x"ffff" & x"ffff";
+    constant or_r3   : std_logic_vector (31 downto 0) := x"ffff" & x"ffff";
+    constant or_z3   : std_logic                      := '0';
+
+    -- Xor test values
+    constant xor_a1   : std_logic_vector (31 downto 0) := x"abcd" & x"ef01";
+    constant xor_b1   : std_logic_vector (31 downto 0) := x"0000" & x"ffff";
+    constant xor_r1   : std_logic_vector (31 downto 0) := x"abcd" & x"10fe";
+    constant xor_z1   : std_logic                      := '0';
+    constant xor_a2   : std_logic_vector (31 downto 0) := x"ffff" & x"ffff";
+    constant xor_b2   : std_logic_vector (31 downto 0) := x"ffff" & x"ffff";
+    constant xor_r2   : std_logic_vector (31 downto 0) := x"0000" & x"0000";
+    constant xor_z2   : std_logic                      := '1';
+    constant xor_a3   : std_logic_vector (31 downto 0) := x"1234" & x"5678";
+    constant xor_b3   : std_logic_vector (31 downto 0) := x"ffff" & x"ffff";
+    constant xor_r3   : std_logic_vector (31 downto 0) := x"edcb" & x"a987";
+    constant xor_z3   : std_logic                      := '0';
+
+    -- sll test values
+    -- abcdef01 << 4
+    constant sll_a1   : std_logic_vector (31 downto 0) := x"abcd" & x"ef01";
+    constant sll_b1   : std_logic_vector (31 downto 0) := x"0000" & x"0004";
+    constant sll_r1   : std_logic_vector (31 downto 0) := x"bcde" & x"f010";
+    constant sll_z1   : std_logic                      := '0';
+    -- ffff ffff << 32
+    constant sll_a2   : std_logic_vector (31 downto 0) := x"ffff" & x"ffff";
+    constant sll_b2   : std_logic_vector (31 downto 0) := x"0000" & x"0020";
+    constant sll_r2   : std_logic_vector (31 downto 0) := x"0000" & x"0000";
+    constant sll_z2   : std_logic                      := '1';
+    -- 0000 ffff << 16
+    constant sll_a3   : std_logic_vector (31 downto 0) := x"0000" & x"ffff";
+    constant sll_b3   : std_logic_vector (31 downto 0) := x"0000" & x"0010";
+    constant sll_r3   : std_logic_vector (31 downto 0) := x"ffff" & x"0000";
+    constant sll_z3   : std_logic                      := '0';
+
+    -- srl test values
+    -- abcd ef01 >> 4
+    constant srl_a1   : std_logic_vector (31 downto 0) := x"abcd" & x"ef01";
+    constant srl_b1   : std_logic_vector (31 downto 0) := x"0000" & x"0004";
+    constant srl_r1   : std_logic_vector (31 downto 0) := x"0abc" & x"def0";
+    constant srl_z1   : std_logic                      := '0';
+    -- ffff ffff >> 32
+    constant srl_a2   : std_logic_vector (31 downto 0) := x"ffff" & x"ffff";
+    constant srl_b2   : std_logic_vector (31 downto 0) := x"0000" & x"0020";
+    constant srl_r2   : std_logic_vector (31 downto 0) := x"0000" & x"0000";
+    constant srl_z2   : std_logic                      := '1';
+    -- ffff 0000 >> 16
+    constant srl_a3   : std_logic_vector (31 downto 0) := x"ffff" & x"0000";
+    constant srl_b3   : std_logic_vector (31 downto 0) := x"0000" & x"0010";
+    constant srl_r3   : std_logic_vector (31 downto 0) := x"0000" & x"ffff";
+    constant srl_z3   : std_logic                      := '0';
+
+    -- sra test values
+    -- abcd ef01 >> 4
+    constant sra_a1   : std_logic_vector (31 downto 0) := x"abcd" & x"ef01";
+    constant sra_b1   : std_logic_vector (31 downto 0) := x"0000" & x"0004";
+    constant sra_r1   : std_logic_vector (31 downto 0) := x"fabc" & x"def0";
+    constant sra_z1   : std_logic                      := '0';
+    -- ffff ffff >> 32
+    constant sra_a2   : std_logic_vector (31 downto 0) := x"ffff" & x"ffff";
+    constant sra_b2   : std_logic_vector (31 downto 0) := x"0000" & x"0020";
+    constant sra_r2   : std_logic_vector (31 downto 0) := x"ffff" & x"ffff";
+    constant sra_z2   : std_logic                      := '0';
+    -- 0000 ff00 >> 16
+    constant sra_a3   : std_logic_vector (31 downto 0) := x"0000" & x"ff00";
+    constant sra_b3   : std_logic_vector (31 downto 0) := x"0000" & x"0010";
+    constant sra_r3   : std_logic_vector (31 downto 0) := x"0000" & x"0000";
+    constant sra_z3   : std_logic                      := '1';
+
     -- Input signals
     signal a, b       : std_logic_vector (31 downto 0);
     signal opcode     : std_logic_vector (3  downto 0);
@@ -118,6 +211,108 @@ begin
         wait for interval;
         assert res  = sub_r3 report "Erro no res teste sub 3";
         assert zero = sub_z3 report "Erro no res teste sub 3";
+        -- and tests
+        opcode <= and_o;
+        a      <= and_a1;
+        b      <= and_b1;
+        wait for interval;
+        assert res  = and_r1 report "Erro no res teste and 1";
+        assert zero = and_z1 report "Erro no res teste and 1";
+        a      <= and_a2;
+        b      <= and_b2;
+        wait for interval;
+        assert res  = and_r2 report "Erro no res teste and 2";
+        assert zero = and_z2 report "Erro no res teste and 2";
+        a      <= and_a3;
+        b      <= and_b3;
+        wait for interval;
+        assert res  = and_r3 report "Erro no res teste and 3";
+        assert zero = and_z3 report "Erro no res teste and 3";
+        -- or tests
+        opcode <= or_o;
+        a      <= or_a1;
+        b      <= or_b1;
+        wait for interval;
+        assert res  = or_r1 report "Erro no res teste or 1";
+        assert zero = or_z1 report "Erro no res teste or 1";
+        a      <= or_a2;
+        b      <= or_b2;
+        wait for interval;
+        assert res  = or_r2 report "Erro no res teste or 2";
+        assert zero = or_z2 report "Erro no res teste or 2";
+        a      <= or_a3;
+        b      <= or_b3;
+        wait for interval;
+        assert res  = or_r3 report "Erro no res teste or 3";
+        assert zero = or_z3 report "Erro no res teste or 3";
+        -- xor tests
+        opcode <= xor_o;
+        a      <= xor_a1;
+        b      <= xor_b1;
+        wait for interval;
+        assert res  = xor_r1 report "Erro no res teste xor 1";
+        assert zero = xor_z1 report "Erro no res teste xor 1";
+        a      <= xor_a2;
+        b      <= xor_b2;
+        wait for interval;
+        assert res  = xor_r2 report "Erro no res teste xor 2";
+        assert zero = xor_z2 report "Erro no res teste xor 2";
+        a      <= xor_a3;
+        b      <= xor_b3;
+        wait for interval;
+        assert res  = xor_r3 report "Erro no res teste xor 3";
+        assert zero = xor_z3 report "Erro no res teste xor 3";
+        -- sll tests
+        opcode <= sll_o;
+        a      <= sll_a1;
+        b      <= sll_b1;
+        wait for interval;
+        assert res  = sll_r1 report "Erro no res teste sll 1";
+        assert zero = sll_z1 report "Erro no res teste sll 1";
+        a      <= sll_a2;
+        b      <= sll_b2;
+        wait for interval;
+        assert res  = sll_r2 report "Erro no res teste sll 2";
+        assert zero = sll_z2 report "Erro no res teste sll 2";
+        a      <= sll_a3;
+        b      <= sll_b3;
+        wait for interval;
+        assert res  = sll_r3 report "Erro no res teste sll 3";
+        assert zero = sll_z3 report "Erro no res teste sll 3";
+        -- srl tests
+        opcode <= srl_o;
+        a      <= srl_a1;
+        b      <= srl_b1;
+        wait for interval;
+        assert res  = srl_r1 report "Erro no res teste srl 1";
+        assert zero = srl_z1 report "Erro no res teste srl 1";
+        a      <= srl_a2;
+        b      <= srl_b2;
+        wait for interval;
+        assert res  = srl_r2 report "Erro no res teste srl 2";
+        assert zero = srl_z2 report "Erro no res teste srl 2";
+        a      <= srl_a3;
+        b      <= srl_b3;
+        wait for interval;
+        assert res  = srl_r3 report "Erro no res teste srl 3";
+        assert zero = srl_z3 report "Erro no res teste srl 3";
+        -- sra tests
+        opcode <= sra_o;
+        a      <= sra_a1;
+        b      <= sra_b1;
+        wait for interval;
+        assert res  = sra_r1 report "Erro no res teste sra 1";
+        assert zero = sra_z1 report "Erro no res teste sra 1";
+        a      <= sra_a2;
+        b      <= sra_b2;
+        wait for interval;
+        assert res  = sra_r2 report "Erro no res teste sra 2";
+        assert zero = sra_z2 report "Erro no res teste sra 2";
+        a      <= sra_a3;
+        b      <= sra_b3;
+        wait for interval;
+        assert res  = sra_r3 report "Erro no res teste sra 3";
+        assert zero = sra_z3 report "Erro no res teste sra 3";
     end process;
 
 end architecture;
